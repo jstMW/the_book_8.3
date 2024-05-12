a8161b22bf3063281cc9642d81f0d97efbfc4b3a
me <github.untracked866@passmail.net>
2024-05-12 09:25:32 -0500

    lots of things but I didn't record them seperately.

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,29 @@
-use std::{collections::HashMap, hash::Hash};
+use std::collections::HashMap;
 
-//list of integers, use a vector and return the median
-//(when sorted, the value in the middle position) and
-//mode (the value that occurs most often; a hash map will be helpful here) of the list.
-
-fn median(v: &Vec<usize>) -> f32 {
+fn median(v: &mut Vec<usize>) -> f32 {
+    v.sort();
     let len: usize = v.len();
-    if len % 2 == 0 {
-        (v[len / 2 - 1] + v[len / 2]) as f32 / 2.0
+    if len%2==0 {
+        (v[len/2 - 1] + v[len/2]) as f32 / 2.0
     } else {
         v[(len + 1) / 2 - 1] as f32
     }
+    
 }
 
 fn mode(v: &Vec<usize>) -> usize {
     let mut mode: usize = 0;
-    let mut hm: HashMap<usize, usize> = HashMap::new();
+    let mut hm: HashMap<usize, usize>  = HashMap::new();
 
     for element in v {
         let count = hm.entry(*element).or_insert(0);
         *count += 1;
     }
 
-    for (element, rep) in hm {
-        if rep > mode {
-            mode = element;
-        }
-    }
+    hm.iter()
+        .max_by_key(|&(key, val)| val)
+        .map(|(&key, value)| key).unwrap()
 
-    mode
 }
 
 #[cfg(test)]
@@ -39,7 +34,7 @@ pub mod test {
         let mut ints: Vec<usize> = Vec::new();
         ints.push(1);
 
-        assert_eq!(median(&ints), 1.0);
+        assert_eq!(median(&mut ints), 1.0);
     }
 
     #[test]
@@ -49,7 +44,7 @@ pub mod test {
         ints.push(2);
         ints.push(3);
 
-        assert_eq!(median(&ints), 2.0);
+        assert_eq!(median(&mut ints), 2.0);
     }
 
     #[test]
@@ -60,8 +55,9 @@ pub mod test {
         ints.push(3);
         ints.push(4);
 
-        assert_eq!(median(&ints), 2.5);
+        assert_eq!(median(&mut ints), 2.5);
     }
+
 
     #[test]
     fn mode_even_element_panic() {
@@ -97,3 +93,4 @@ pub mod test {
         assert_eq!(mode(&ints), 0); /*actually can't determine it for now!! */
     }
 }
+
