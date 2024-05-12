4e03d288c8c9aeb01fbf87abbd0cffeb817670fc
me <github.untracked866@passmail.net>
2024-05-12 15:46:11 -0500

    change the logic a little bit in mode

--- src/mode.rs
+++ src/mode.rs
@@ -3,9 +3,18 @@ pub fn mode(v: &[usize]) -> Option<usize> {
     let mut hm: HashMap<usize, usize> = HashMap::new();
 
     for element in v {
-        let count = hm.entry(*element).or_insert(0);
+        let count = hm
+            .entry(*element)
+            .or_insert(0);
         *count += 1;
     }
 
-    hm.iter().max_by_key(|&(_, val)| val).map(|(&key, _)| key)
+    match hm.iter()
+        .max_by_key(|&(_, val)| val)
+        .map(|(&key, _)| key) {
+            None => None, 
+            Some(1) => None, 
+            Some(x) => Some(x),
+        }
+
 }
