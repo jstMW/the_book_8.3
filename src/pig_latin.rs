04fc230b0a81a5433edcb7443c4183e710187a1d
jst-MW <unknown>
2024-05-15 22:01:51 -0500

    went a little forward, but actually nothing :(

--- src/pig_latin.rs
+++ src/pig_latin.rs
@@ -1,3 +1,26 @@
+pub const wovels: Vec<char> = vec!['a', 'e', 'i', 'o', 'u'];
 pub fn to_pig_latin(s: &str) -> &str {
+    let is_wovel: bool = is_first_wovel(s);
+    let new_s = s.clone().to_string().push_str("-hay");
+    if is_wovel {
+        return make_first_wovel(s);
+    } else {
+    }
     s
+}
+
+fn is_first_wovel(word: &str) -> bool {
+    let first_letter = word.chars().next().unwrap();
+
+    for letter in wovels {
+        if first_letter == letter {
+            return true
+        }
+    }
+
+    false
+}
+
+fn make_first_wovel(s: &str) -> &str {
+    
 }
